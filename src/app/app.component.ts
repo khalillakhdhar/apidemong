c367b32eb901b75ae688cdd64d399fa4237e1666
khalil <khalillakhdharatc@gmail.com>
2023-06-18 10:36:32 +0100

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TodoService } from './shared/services/todo.service';
+import { Todo } from './shared/classes/todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'apidemong';
+  todos:Todo[]=[];
+  constructor(private todoService:TodoService)
+  {
+
+  }
+  ngOnInit() {
+    this.todoService.getTodos().subscribe((datas)=>
+    {
+      this.todos = datas;
+      console.log("liste",this.todos);
+    }
+
+    )
+  }
 }
